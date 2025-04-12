9d4f4ec458c7eae74b085e1fca1ead4a9c2f2c34
Ranji <ranjinie.souria@gmail.com>
2025-04-12 18:27:34 +0200

    Added prettier, nav bar component and button components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const RoutePaths = {
+    HOME: 'home',
+    ABOUT: 'about',
+    CONTACT: 'contact'
+
+  };
+  
+
+export const routes: Routes = [
+    { path: '', component: AppComponent },
+];
