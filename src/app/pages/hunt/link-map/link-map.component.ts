0e7f12f0416c8cbbfb19f761162ecd60d1577ed3
Ranji <ranjinie.souria@gmail.com>
2025-07-13 12:45:03 +0200

    map component

---
+++ src/app/pages/hunt/link-map/link-map.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-link-map',
+  imports: [],
+  templateUrl: './link-map.component.html',
+  styleUrl: './link-map.component.scss'
+})
+export class LinkMapComponent {
+
+}
