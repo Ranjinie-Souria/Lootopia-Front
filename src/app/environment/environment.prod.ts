86c30d305d931411940100532fb257e752fb80f5
Mathieu MORGAT <m.morgat@bweez.com>
2025-07-14 06:12:38 +0200

    cicd(ci): add .htaccess + urls in environnment

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api',
-  mapboxAccessToken: 'pk.eyJ1IjoibW1vcmdhdHNkdiIsImEiOiJjbWI3bjVtZ3cwYXNuMmxzNnY0bWpiMHU5In0.vnwcBCFV0_FxercRsaVqUg',
+  apiUrl: 'https://backlootopia.mmorgat.com/api',
+  mapboxAccessToken:
+    'pk.eyJ1IjoibW1vcmdhdHNkdiIsImEiOiJjbWI3bjVtZ3cwYXNuMmxzNnY0bWpiMHU5In0.vnwcBCFV0_FxercRsaVqUg',
 };
